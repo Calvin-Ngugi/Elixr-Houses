9e83ddbdb17f76fde382657367855de0d064c7b8
Calvin Njuguna <ncalvin67@gmail.com>
2022-02-18 21:39:27 +0300

    commit message

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/interior.jpg'
+import ImageTwo from '../images/house5.jpg'
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Adipisicing elit qui enim incididunt nostrud. Occaecat dolor veniam mollit ipsum deserunt nostrud fugiat duis. Duis Lorem non excepteur tempor sint non sunt nostrud minim anim eiusmod cupidatat fugiat. Commodo nostrud aute cupidatat mollit est elit mollit tempor consequat tempor irure adipisicing elit fugiat. Eu officia laborum amet pariatur proident qui velit.',
+    paragraphTwo: 'Magna anim dolore et adipisicing velit sit tempor minim laboris deserunt ex commodo adipisicing et. Laborum sint do aliquip non nostrud aute eu mollit cillum do amet ut fugiat ad. Et ipsum magna dolor deserunt eiusmod quis ex. Enim ullamco irure ea ea nulla deserunt consequat labore sit ad. Et veniam deserunt qui deserunt. Elit do exercitation deserunt fugiat enim ad pariatur proident consectetur esse proident id elit proident.',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+}
+
+export const InfoDataTwo = {
+    heading: 'Modern designs',
+    paragraphOne: 'Ea ex fugiat qui adipisicing nulla ut. Magna cillum nulla ut occaecat ullamco Lorem proident nulla. Irure culpa labore duis et ipsum laboris ea ad culpa reprehenderit reprehenderit.',
+    paragraphTwo: 'Pariatur sunt consequat cillum adipisicing irure exercitation laboris cillum magna. Reprehenderit do anim adipisicing qui.',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300
+}
